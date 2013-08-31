556c0d206011f12cf540228ee4f0379a990d8dae
patz <patz@localhost.localdomain>
2013-08-31 21:52:16 +0800

    inequality test for Meter

--- length-calculator/src/test/scala/LengthCalculator.scala
+++ length-calculator/src/test/scala/LengthCalculator.scala
@@ -2,13 +2,16 @@ package learnscala.lengthcalculator.test
 
 import learnscala.lengthcalculator._
 
-import org.scalatest._
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSuite
 
-class LengthCalculatorTest extends FlatSpec with ShouldMatchers {
+class LengthCalculatorTest extends FunSuite {
 
-  "Meter instances with same value" should "be equal" in {
-    new Meter(1) should equal (new Meter(1))
+  test("Meter instances with same value should be equal") {
+    assert(new Meter(1) == new Meter(1))
+  }
+
+  test("Meter instances with different value should not be equal") {
+    assert(new Meter(2) != new Meter(1))
   }
 
 }
