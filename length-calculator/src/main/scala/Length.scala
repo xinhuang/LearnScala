3d9d6aa1aa02302b43da48c608f8f5a51107d418
Xin Huang <walkaniceday@gmail.com>
2013-09-01 23:07:36 +0800

    use look-up table to get unit-to-meter ratio

--- length-calculator/src/main/scala/Length.scala
+++ length-calculator/src/main/scala/Length.scala
@@ -5,15 +5,26 @@ object Length {
   implicit def doubleExt(value: Double) = new Number2Meter(value)
 
   class Number2Meter(val value: Double) {
-    def meter: Meter = new Meter(value)
+    import collection.mutable.Map
+    private val units:Map[String, Double] = Map(
+      "meter"       -> 1,
+      "decimeter"   -> 0.1,
+      "centimeter"  -> 0.01,
+      "inch"        -> 0.0254
+    )
+    private def create(name:String):Meter = {
+      new Meter(value * units(name))
+    }
+
+    def meter: Meter = create("meter")
     def m = meter
 
-    def centimeter = 0.01.m * value
+    def centimeter = create("centimeter")
     def cm = centimeter
 
-    def decimeter: Meter = 0.1.m * value
+    def decimeter: Meter = create("decimeter")
 
-    def inch: Meter = 0.0254.m * value
+    def inch: Meter = create("inch")
   }
 
   val precision = 0.00001.m
