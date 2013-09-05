35740b5740dca5c58189329c3edfd9287dadf709
Patz Huang <walkaniceday@gmail.com>
2013-09-05 17:03:59 +0800

    refactor test

--- parking-lot/src/test/scala/ParkingLotTest.scala
+++ parking-lot/src/test/scala/ParkingLotTest.scala
@@ -6,15 +6,13 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
 class ParkingLotTest extends FlatSpec with ShouldMatchers {
-  "empty lot" should "have size equals 0" in {
-  	val sut = new ParkingLot(3)
+  val sut = new ParkingLot(3)
 
+  "empty lot" should "have size equals 0" in {
   	sut.size should be (0)
   }
 
   "parking lot size" should "be 1 after 1 car parked" in {
-  	val sut = new ParkingLot(3)
-
   	sut park new Car
 
   	sut.size should be (1)
