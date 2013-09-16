fa976649f7d025489fe50f46e1d3c647bc39e91c
Patz Huang <walkaniceday@gmail.com>
2013-09-16 23:59:55 +0800

    refactor

--- command-line/src/main/scala/CommandLine.scala
+++ command-line/src/main/scala/CommandLine.scala
@@ -2,13 +2,9 @@ package learnscala.commandline
 
 import scala.reflect.runtime.universe
 import scala.reflect.Manifest
-import scala.reflect.api.Mirror
-import scala.reflect.api.Mirrors
-import scala.annotation.StaticAnnotation
 
 object CommandLine {
-  val ru = reflect.runtime.universe
-  val rm = ru.runtimeMirror(getClass.getClassLoader)
+  private val rm = universe.runtimeMirror(getClass.getClassLoader)
 
   def parse[T](args: Seq[String])(implicit m: Manifest[T]): T = {
     val option = m.runtimeClass.newInstance.asInstanceOf[T]
@@ -16,14 +12,14 @@ object CommandLine {
     val name = args(0).drop(2)
     val value = args(1)
 
-    setField[T](option, name, value)
+    val im = rm.reflect(option)
+    setField[T](im, name, value)
 
   	option
   }
 
-  private def setField[T](instance: T, name: String, value: String)(implicit m: Manifest[T]) {
-    val im = rm.reflect(instance)
-    val field = ru.typeOf[T].declaration(ru.newTermName(name)).asTerm.accessed.asTerm
+  private def setField[T](im: universe.InstanceMirror, name: String, value: String)(implicit m: Manifest[T]) {
+    val field = universe.typeOf[T].declaration(universe.newTermName(name)).asTerm.accessed.asTerm
     val fm = im.reflectField(field)
     fm.set(value)
   }
