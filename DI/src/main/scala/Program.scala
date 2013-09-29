2d026d43f89ca897d89b217fe5004a2623ba7d50
Patz Huang <walkaniceday@gmail.com>
2013-09-29 14:38:27 +0800

    add StructuralTyping

--- DI/src/main/scala/Program.scala
+++ DI/src/main/scala/Program.scala
@@ -1,73 +1,13 @@
 package learnscala.di
 
-/******************************************************
-                       Interface
- ******************************************************/
-
-trait OnOffDeviceComponent {
-  val onOff: OnOffDevice
-  trait OnOffDevice {
-  	def on: Unit
-  	def off: Unit
-  }
-}
-
-trait SensorDeviceComponent {
-  val sensor: SensorDevice
-  trait SensorDevice {
-    def isCoffeePresent: Boolean
-  }
-}
-
-
-/******************************************************
-                      Implementation
- ******************************************************/
-
-trait OnOffDeviceComponentImpl extends OnOffDeviceComponent {
-  class Heater extends OnOffDevice {
-    def on = println("heater.on")
-    def off = println("heater.off")
-  }
-}
-
-trait SensorDeviceComponentImpl extends SensorDeviceComponent {
-  class PotSensor extends SensorDevice {
-    def isCoffeePresent = true
-  }
-}
-
-/******************************************************
-                       Inject Point
- ******************************************************/
-
-trait WarmerComponentImpl {
-  this: SensorDeviceComponent with OnOffDeviceComponentImpl =>
-  class Warmer {
-    def trigger = {
-      if (sensor.isCoffeePresent) onOff.on
-      else onOff.off
-    }
-  }
-}
-
-/******************************************************
-                       Configuration
- ******************************************************/
-
-object ComponentRegistry extends
-  OnOffDeviceComponentImpl with
-  SensorDeviceComponentImpl with
-  WarmerComponentImpl {
-
-  val onOff = new Heater
-  val sensor = new PotSensor
-  val warmer = new Warmer
-}
-
 object Program {
   def main(args: Array[String]): Unit = {
-    val warmer = ComponentRegistry.warmer
+
+    println("Cake Pattern:")
+    val warmer = cakepattern.ComponentRegistry.warmer
     warmer.trigger
+
+    println("Structural Typing:")
+    new structuraltyping.Client(structuraltyping.Config)
   }
 }
---
+++ DI/src/main/scala/StructuralTyping.scala
@@ -0,0 +1,39 @@
+package learnscala.di.structuraltyping
+
+trait OnOffDevice {
+  def on: Unit
+  def off: Unit
+}
+
+trait SensorDevice {
+  def isCoffeePresent: Boolean
+}
+
+class Heater extends OnOffDevice {
+  def on = println("heater.on")
+  def off = println("heater.off")
+}
+
+class PotSensor extends SensorDevice {
+  def isCoffeePresent = true
+}
+
+class Warmer(env: {
+  val potSensor: SensorDevice
+  val heater: OnOffDevice
+  }) {
+  def trigger = {
+    if (env.potSensor.isCoffeePresent) env.heater.on
+    else env.heater.off
+  }
+}
+
+class Client(env: { val warmer: Warmer }) {
+  env.warmer.trigger
+}
+
+object Config {
+  lazy val potSensor = new PotSensor
+  lazy val heater = new Heater
+  lazy val warmer = new Warmer(this)
+}
